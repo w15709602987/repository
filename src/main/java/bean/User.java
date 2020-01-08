ad0186a9393e40c83787187920801c41ceaada31
weifeng <1597759516@qq.com>
2020-01-08 15:44:14 +0800

    0.2

---
+++ src/main/java/bean/User.java
@@ -0,0 +1,12 @@
+package bean;
+
+public class User {
+    private String id;
+    private String name;
+    public String getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+}
